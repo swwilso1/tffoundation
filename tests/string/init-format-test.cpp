8b7678dfe22d00e268d7f258f18927b310cbb204
Steve Wilson <swwilso1@gmail.com>
2017-09-16 17:35:56 -0600

    Add tests for String::initWithFormat.

--- tests/string/init-format-test.cpp
+++ tests/string/init-format-test.cpp
@@ -49,3 +49,176 @@ TEST(StringTest, DecimalNumberTest2)
     String t("2147483647ten");
     EXPECT_TRUE(s == t);
 }
+
+
+TEST(StringTest, DecimalNumberTest3)
+{
+    int f {-1};
+    String s = String::initWithFormat("%d", f);
+    String t = String("-1");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, OctalNumberTest)
+{
+    int f{10};
+    String s = String::initWithFormat("%o", f);
+    String t("12");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, OctalNumberTest2)
+{
+    int f {2147483647};
+    String s = String::initWithFormat("%ooctal", f);
+    String t("17777777777octal");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, OctalNumberTest3)
+{
+    int f {-8};
+    String s = String::initWithFormat("%o", f);
+    String t("37777777770");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, LowerCaseHexNumberTest)
+{
+    int f {10};
+    String s = String::initWithFormat("%x",f);
+    String t("a");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, LowerCaseHexNumberTest2)
+{
+    int f {2147483647};
+    String s = String::initWithFormat("%xhex", f);
+    String t = String("7fffffffhex");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, LowerCaseHexNumberTest3)
+{
+    int f {-16};
+    String s = String::initWithFormat("%x", f);
+    String t = String("fffffff0");
+    EXPECT_TRUE(s == t);
+}
+
+
+
+TEST(StringTest, UpperCaseHexNumberTest)
+{
+    int f {10};
+    String s = String::initWithFormat("%X",f);
+    String t("A");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, UpperCaseHexNumberTest2)
+{
+    int f {2147483647};
+    String s = String::initWithFormat("%Xhex", f);
+    String t("7FFFFFFFhex");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, UpperCaseHexNumberTest3)
+{
+    int f {-16};
+    String s = String::initWithFormat("%X", f);
+    String t("FFFFFFF0");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, UnsignedIntNumberTest)
+{
+    unsigned int f {2147483648};
+    String s = String::initWithFormat("%u", f);
+    String t("2147483648");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, UnsignedIntNumberTest2)
+{
+    unsigned int f {4294967295};
+    String s = String::initWithFormat("%u", f);
+    String t("4294967295");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, UnsignedLongNumberTest)
+{
+    unsigned long f {4294967296};
+    String s = String::initWithFormat("%U", f);
+    String t("4294967296");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, UnsignedLongNumberTest2)
+{
+    unsigned long f {18446744073709551615UL};
+    String s = String::initWithFormat("%U", f);
+    String t("18446744073709551615");
+    EXPECT_TRUE(s == t);
+}
+
+
+TEST(StringTest, SingleCharacterTest)
+{
+    char c = '9';
+    String s = String::initWithFormat("%c", c);
+    String t("9");
+    EXPECT_EQ(s,t);
+}
+
+
+TEST(StringTest, CStyleStringTest)
+{
+    String s = String::initWithFormat("%s", "Hello World");
+    String t("Hello World");
+    EXPECT_EQ(s,t);
+}
+
+
+TEST(StringTest, STLStyleStringTest)
+{
+    std::string j("Hello World");
+    String s = String::initWithFormat("%S", j);
+    String t("Hello World");
+    EXPECT_EQ(s,t);
+}
+
+
+TEST(StringTest, FRealNumberTest)
+{
+    float f {3.141561};
+    String s = String::initWithFormat("%f", f);
+    String t("3.141561");
+    EXPECT_EQ(s,t);
+}
+
+
+TEST(StringTest, GRealNumberTest)
+{
+    float f {3.141561};
+    String s = String::initWithFormat("%g", f);
+    String t("3.14156");
+    EXPECT_EQ(s,t);
+}
+
+
