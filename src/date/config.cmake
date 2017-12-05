8b9b8eb1d8c532dbd68e76c14fa338469fa7a095
Steve Wilson <swilson@dialight.com>
2017-12-05 08:05:26 -0700

    Make sure to add tfdateformatter.cxx/hxx to header files/source files.

--- src/date/config.cmake
+++ src/date/config.cmake
@@ -14,6 +14,8 @@ list(APPEND FOUNDATION_HEADER_FILES
         ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdateclocks.hpp
         ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdatecomponent.cxx
         ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdatecomponent.hxx
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdateformatter.cxx
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdateformatter.hxx
         ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdatesymbol.cxx
         ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdatesymbol.hxx
         ${CMAKE_CURRENT_SOURCE_DIR}/src/date/tfdatetypes.hpp
@@ -27,6 +29,8 @@ list(APPEND FOUNDATION_SOURCE_FILES
         src/date/tfdateclocks.hpp
         src/date/tfdatecomponent.cxx
         src/date/tfdatecomponent.hxx
+        src/date/tfdateformatter.cxx
+        src/date/tfdateformatter.hxx
         src/date/tfdatesymbol.cxx
         src/date/tfdatesymbol.hxx
         src/date/tfdatetypes.cpp
@@ -41,6 +45,8 @@ set_source_files_properties(
         "src/date/tfdate.hxx"
         "src/date/tfdatecomponent.cxx"
         "src/date/tfdatecomponent.hxx"
+        "src/date/tfdateformatter.cxx"
+        "src/date/tfdateformatter.hxx"
         "src/date/tfdatesymbol.cxx"
         "src/date/tfdatesymbol.hxx"
         PROPERTIES
