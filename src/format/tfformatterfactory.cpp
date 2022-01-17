eb18e9df13ba5a6c6dffb6833e5c07a9574d2a9c
Steve Wilson <swwilso1@gmail.com>
2022-01-17 13:45:34 -0700

    Update copyrights for 2022.

--- src/format/tfformatterfactory.cpp
+++ src/format/tfformatterfactory.cpp
@@ -2,7 +2,7 @@
 
 Tectiform Open Source License (TOS)
 
-Copyright (c) 2019 Tectiform Inc.
+Copyright (c) 2015 to 2022 Tectiform Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
